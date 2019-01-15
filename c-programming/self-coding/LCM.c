25ff00452537eda99169314258ed84fb91a8870d
tanjinamumu <46072784+tanjinamumu@users.noreply.github.com>
2019-01-15 22:14:47 +0600

    Add files via upload

---
+++ c-programming/self-coding/LCM.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main()
+{
+    int a, b, x, gcd, lcm;
+    scanf("%d %d", &a, &b);
+    if(a<b)
+    {
+        x=a;
+    }
+    else
+    {
+        x=b;
+    }
+    for(; x>=1; x--)
+    {
+
+        if(a%x==0 && b%x==0)
+        {
+            gcd=x;
+            break;
+        }
+
+    }
+            printf("GCD is: %d\n",gcd);
+            lcm=(a*b)/gcd;
+            printf("LCM is: %d\n",lcm);
+            return 0;
+
+}
+
