c11a15e0f33db24d63241920d9fd697f81bedc5e
tanjinamumu <46072784+tanjinamumu@users.noreply.github.com>
2019-03-24 14:04:46 +0600

    Add files via upload

---
+++ c-programming/Vjudge/contest7.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int m,n,sum;
+    while(scanf("%d %d",&m,&n)==2)
+        {
+            sum=m*n-1;
+            printf("%d\n",sum);
+        }
+    return 0;
+}
