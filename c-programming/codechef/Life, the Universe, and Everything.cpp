dff4cf74b75e949584a0cff82aad1d09c5fcc5a6
tanjinamumu <46072784+tanjinamumu@users.noreply.github.com>
2020-04-18 10:53:51 +0600

    Add files via upload

---
+++ c-programming/codechef/Life, the Universe, and Everything.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int nc;
+    while(cin>>nc)
+    {
+        if(nc==42)
+        {
+            break;
+        }
+    cout<<nc<<endl;
+    }
+    return 0;
+}
