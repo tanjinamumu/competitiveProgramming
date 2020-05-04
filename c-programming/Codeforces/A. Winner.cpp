93d4d94f1fd0a00f85ecebfe4bf22f6245dd40b5
tanjinamumu <46072784+tanjinamumu@users.noreply.github.com>
2020-05-04 10:01:06 +0600

    Add files via upload

---
+++ c-programming/Codeforces/A. Winner.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    while(n--)
+    {
+        int score;
+        string name;
+        cin>>name>>score;
+        if()
+
+    }
+
+}
