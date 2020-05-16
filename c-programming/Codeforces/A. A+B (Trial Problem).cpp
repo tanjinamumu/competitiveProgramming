66fbde934e59a4a5d6147034e9fb72b7bde2b768
tanjinamumu <46072784+tanjinamumu@users.noreply.github.com>
2020-05-16 12:16:19 +0600

    Add files via upload

---
+++ c-programming/Codeforces/A. A+B (Trial Problem).cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+
+    int t,i,sum;
+    cin>>t;
+    while(t--)
+    {
+        int a,b;
+        cin>>a>>b;
+        sum=a+b;
+        cout<<sum<<endl;
+    }
+    return 0;
+}
