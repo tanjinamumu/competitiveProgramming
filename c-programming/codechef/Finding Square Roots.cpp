da7a224f7c1967a7066c3921622d672505069eb2
tanjinamumu <46072784+tanjinamumu@users.noreply.github.com>
2020-05-27 22:16:21 +0600

    Add files via upload

---
+++ c-programming/codechef/Finding Square Roots.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t,n,root;
+    cin>>t;
+    while(t--)
+    {
+        cin>>n;
+        root=sqrt(n);
+        cout<<root<<endl;
+    }
+
+
+
+}
+
+
