69332df1007740786d25570256ae7c96db844f16
tanjinamumu <46072784+tanjinamumu@users.noreply.github.com>
2020-06-02 13:37:00 +0600

    Add files via upload

---
+++ c-programming/uva/10302 - Summation of Polynomials.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    long long int n;
+    while (cin >> n)
+    {
+        cout<<((n*(n+1))/2)*((n*(n+1))/2)<<endl;
+    }
+    return 0;
+}
+
+
