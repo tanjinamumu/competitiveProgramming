3777152e719190c8b79fff48b98a1d7b2ed940b8
tanjinamumu <46072784+tanjinamumu@users.noreply.github.com>
2020-06-05 10:07:46 +0600

    Add files via upload

---
+++ c-programming/codechef/Valid Triangles.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int tc,a,b,c,sum;
+    cin>>tc;
+    while(tc--)
+    {
+        cin>>a>>b>>c;
+        if((a+b+c)==180)
+        {
+            cout<<"YES"<<endl;
+        }
+        else
+        {
+            cout<<"NO"<<endl;
+        }
+    }
+
+}
