ad782f141ecf72a46ef5c6ddf359a92cbe51df50
tanjinamumu <46072784+tanjinamumu@users.noreply.github.com>
2020-07-02 20:32:40 +0600

    Add files via upload

---
+++ c-programming/atcoder/A.Odds of Oddness.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n,s;
+    double ans;
+    cin>>n;
+    s=(n+1)/2;
+    //cout<<s<<endl;
+    ans= (double)s/n;
+    cout<<fixed<<setprecision(9)<<ans<<endl;
+
+}
