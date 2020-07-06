9e10888ff6155a9caf397c3531f9c5668ec30430
tanjinamumu <46072784+tanjinamumu@users.noreply.github.com>
2020-07-06 20:01:37 +0600

    Add files via upload

---
+++ c-programming/atcoder/At-A.payment.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+ int n;
+ cin >> n;
+ int m=n%1000;
+ //cout<<m<<" ";
+ int o=(1000-m);
+ //cout<<o<<" ";
+ int p=(o%1000);
+ cout<<p<<endl;
+}
