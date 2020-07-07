2cf830d9d7ec0bceb99a90a34ba34f3373d4f29d
tanjinamumu <46072784+tanjinamumu@users.noreply.github.com>
2020-07-07 11:23:27 +0600

    Add files via upload

---
+++ c-programming/Numerical_Method/1.Bisection method.cpp
@@ -0,0 +1,33 @@
+//Find a real root of the equation f(x) = x^3 − x − 1 = 0 using bisection method.
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int i,n=30;
+    float a,b,x,y;
+    a=1;
+    b=2;
+    for (i=1; i<n; i++)
+    {
+        x=(a+b)/2;
+        y= x*x*x-x-1;
+        if (y==0)
+        {
+            cout<<"The root is x"<<" "<<i<<" "<<"="<<" "<<x<<endl;
+        }
+        else if(y>0)
+        {
+            a=a;
+            b=x;
+            cout<<"The root is x"<<" "<<i<<" "<<"="<<" "<<x<<endl;
+        }
+        else
+        {
+            a=x;
+            b=b;
+            cout<<"The root is x"<<" "<<i<<" "<<"="<<" "<<x<<endl;
+        }
+    }
+    return 0;
+}
