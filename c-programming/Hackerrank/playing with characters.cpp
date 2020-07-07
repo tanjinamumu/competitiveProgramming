e02948442c102bab3962e1d817d45d5814d38460
tanjinamumu <46072784+tanjinamumu@users.noreply.github.com>
2020-07-07 11:22:40 +0600

    Add files via upload

---
+++ c-programming/Hackerrank/playing with characters.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    char ch, s[8],sen[50];
+    scanf("%c", &ch);
+    printf("%c\n",ch);
+    scanf(" %[^\n]s",s);
+    printf("%s\n",s);
+    scanf(" %[^\n]s",sen);
+    printf("%s", sen);
+    return 0;
+}
