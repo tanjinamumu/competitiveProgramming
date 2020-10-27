b2957ad1c68c825469df536841431ab5c3297da0
Tanjina Mumu <46072784+tanjinamumu@users.noreply.github.com>
2020-10-27 08:35:25 +0600

    Add files via upload

---
+++ codeforces/A/A. Protect Sheep.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int r,c,a,b,i,j;
+    char mat[600][600];
+    cin>>r>>c;
+    for(i=0; i<r; i++)
+    {
+        cin>>mat[i];
+    }
+    for(i=0; i<r; i++)
+    {
+        for(j=0; j<c; j++)
+        {
+            if(mat[i][j]=='.')
+            {
+                mat[i][j]='D';
+                //cout<<mat[i][j]<<" ";
+            }
+            if(mat[i][j]=='S')
+            {
+                if(mat[i-1][j]=='W'||mat[i+1][j]=='W'||mat[i][j-1]=='W'||mat[i][j+1]=='W')
+                {
+                    cout<<"No";
+                    return 0;
+                }
+            }
+        }
+    }
+    cout<<"Yes"<<endl;
+    for(i=0; i<r; i++)
+    {
+        cout<<mat[i]<<endl;
+    }
+}
