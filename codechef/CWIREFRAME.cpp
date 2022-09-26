87db11388009c8cf1bd46bf3c40c24d761d937e0
tanjinamumu <tanjinamumu@gmail.com>
2022-09-26 13:34:17 +0600

    code of CWIRFRAME added

---
+++ codechef/CWIREFRAME.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	// your code goes here
+	int t,n,m,x,y,result;
+	cin>>t;
+	while(t--)
+	{
+	    cin>>n>>m>>x;
+	    y = (2*n) + (2*m);
+	    result = x*y;
+	    cout<<result<<endl;
+	}
+	return 0;
+	return 0;
+}
