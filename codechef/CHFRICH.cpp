8c19d10264e4871abb0fb3aa78c9b6cb4720f847
tanjinamumu <tanjinamumu@gmail.com>
2022-10-18 05:08:52 +0600

    solved

---
+++ codechef/CHFRICH.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t,a,b,c,x,d;
+	cin>>t;
+	while(t--)
+	{
+	    cin>>a>>b>>x;
+	    c=b-a;
+	    d=c/x;
+	    cout<<d<<endl;
+	    
+	}
+	return 0;
+}
