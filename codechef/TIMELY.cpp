bda5dc0aced1b197952c78ebab5867b559d52e5d
tanjinamumu <tanjinamumu@gmail.com>
2022-10-18 04:45:34 +0600

    solved

---
+++ codechef/TIMELY.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t,x;
+	cin>>t;
+	while(t--)
+	{
+	    cin>>x;
+	    if(x>=30)
+	    {
+	        cout<<"YES"<<endl;
+	    }
+	    else
+	    {
+	        cout<<"NO"<<endl;
+	    }
+	    
+	}
+	return 0;
+}
