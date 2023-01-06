dac600936a74c9621cb982cb60ddf8bda6636cd1
tanjinamumu <tanjinamumu@gmail.com>
2023-01-06 21:01:37 +0600

    added

---
+++ codechef/HOOPS.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	long long int n; 
+	cin>>t;
+	while(t--)
+	{
+	    cin>>n;
+	    cout<<(n+1)/2<<endl;
+	    
+	    
+	}
+	return 0;
+}
