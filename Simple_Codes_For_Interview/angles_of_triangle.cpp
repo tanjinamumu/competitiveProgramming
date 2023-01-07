bf143bd7c0db0b90e864646477e283c827a7233c
tanjinamumu <tanjinamumu@gmail.com>
2023-01-07 14:27:10 +0600

    added and updated

--- Simple_Codes_For_Interview/angles_of_triangle.cpp
+++ Simple_Codes_For_Interview/angles_of_triangle.cpp
@@ -2,5 +2,10 @@
 using namespace std;
 int main()
 {
-
+    float a,b,c,angleA,angleB,angleC;
+    cin>>a>>b>>c;
+    angleA=((acos((b*b)+(c*c)-(a*a))/(2*b*c))*(180/3.1416));
+    angleB=((acos((a*a)+(c*c)-(b*b))/(2*a*c))*(180/3.1416));
+    angleC=((acos((a*a)+(b*b)-(c*c))/(2*a*b))*(180/3.1416));
+    cout<<angleA<<" "<<angleB<<" "<<angleC<<endl;
 }
