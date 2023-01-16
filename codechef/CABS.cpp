b77b7189bcce17b979ed1530d5d1cfc6b4881b20
tanjinamumu <tanjinamumu@gmail.com>
2023-01-17 01:48:48 +0600

    added

---
+++ codechef/CABS.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t,x,y;
+	cin>>t;
+	while(t--)
+	{
+	    cin>>x>>y;
+	    if(x>y)
+	    {
+	        cout<<"SECOND"<<endl;
+	    }
+	    else if(y>x)
+	    {
+	        cout<<"FIRST"<<endl;
+	    }
+	    else
+	    {
+	        cout<<"ANY"<<endl;
+	    }
+	}
+	return 0;
+}
