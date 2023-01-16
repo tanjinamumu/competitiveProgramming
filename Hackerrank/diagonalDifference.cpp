dbbd6100203f26763b16f6da81f59b1339b82d83
tanjinamumu <tanjinamumu@gmail.com>
2023-01-16 22:04:04 +0600

    added to hackerrank

--- Hackerrank/diagonalDifference.cpp
+++ Hackerrank/diagonalDifference.cpp
@@ -1,40 +1,34 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-int difference(int arr[100][100], int n)
-{
-    int d1 = 0, d2 = 0;
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < n; j++)
-        {
-            if (i == j)
-            {
-                d1 += arr[i][j];
-            }
-
-            if (i == n - j - 1)
-            {
-                d2 += arr[i][j];
-            }
-        }
-    }
-    return abs(d1 - d2);
-}
-
 int main()
 {
     int n;
     cin>>n;
     int arr[100][100];
-    for (int i = 0; i < n; i++)
+    for(int i=0;i<n;i++)
     {
-        for (int j = 0; j < n; j++)
+        for(int j=0;j<n;j++)
         {
             cin>>arr[i][j];
         }
     }
-    cout << difference(arr, n);
+    int dif1 = 0, dif2 = 0;
+    for (int i=0;i<n; i++)
+    {
+        for (int j=0;j<n;j++)
+        {
+            if (i==j)
+            {
+                dif1+=arr[i][j];
+            }
+
+            if (i==n-j-1)
+            {
+                dif2+=arr[i][j];
+            }
+        }
+    }
+    cout<<abs(dif1-dif2)<<endl;
     return 0;
 }
 
