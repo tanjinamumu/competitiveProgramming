b9c88d8c1219714d9f441dd1bf501211adaf2537
tanjinamumu <tanjinamumu@gmail.com>
2023-01-16 22:30:44 +0600

    added to contest vjudge

---
+++ Vjudge/E-LovelyCouple.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n,k,cnt;
+    cin>>n>>k;
+    int arr[n];
+    cnt=0;
+    for(int i=0; i<n; i++)
+    {
+        cin>>arr[i];
+    }
+    for(int i=0; i<n; i++)
+    {
+        for(int j=i+1; j<n; j++)
+        {
+            if((arr[i]+arr[j])%k==0)
+            {
+                cnt++;
+            }
+        }
+    }
+    cout<<cnt<<endl;
+    return 0;
+}
