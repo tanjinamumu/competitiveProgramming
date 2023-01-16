4d1cc048e25c7b6b729cb420d8522883e3211b7c
tanjinamumu <tanjinamumu@gmail.com>
2023-01-17 01:49:16 +0600

    added

---
+++ codechef/CREDSCORE.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	if(t>=750)
+	{
+	    cout<<"YES"<<endl;
+	}
+	else
+	{
+	    cout<<"NO"<<endl;
+	}
+	return 0;
+}
