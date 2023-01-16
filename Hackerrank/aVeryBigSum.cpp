4a291cbe089ba79ee7d2824fec69226c5ca66990
tanjinamumu <tanjinamumu@gmail.com>
2023-01-16 22:01:18 +0600

    added to hackerrank

---
+++ Hackerrank/aVeryBigSum.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    long long int t,n,sum=0;
+    cin>>t;
+    while(t--)
+    {
+        cin>>n;
+        sum+=n;
+    }
+    cout<<sum<<endl;
+    return 0;
+}
