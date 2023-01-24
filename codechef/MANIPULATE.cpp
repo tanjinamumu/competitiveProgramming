01b2050a9d865743ebf95370261cd33312d8f240
tanjinamumu <tanjinamumu@gmail.com>
2023-01-24 23:05:02 +0600

    added

---
+++ codechef/MANIPULATE.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t,x,y,c,d;
+	cin>>t;
+	while(t--)
+	{
+	    cin>>x>>y;
+	    if(x>=y)
+	    {
+	        cout<<"YES"<<endl;
+	    }
+	    else
+	    {
+	        cout<<"NO"<<endl;
+	    }
+	}
+	return 0;
+}
