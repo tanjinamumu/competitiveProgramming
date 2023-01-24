8d12d88a30043eb41cbb694fc763bc850303bb7c
tanjinamumu <tanjinamumu@gmail.com>
2023-01-24 23:04:11 +0600

    added

---
+++ codechef/DETSCORE.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t,x,y,c,d;
+	cin>>t;
+	while(t--)
+	{
+	    cin>>x>>y;
+	    c=x/10;
+	    d=c*y;
+	    cout<<d<<endl;
+	}
+	return 0;
+}
