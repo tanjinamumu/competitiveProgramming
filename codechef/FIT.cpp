3ad705812f40b31f6115d823da3a226bd60fafb6
tanjinamumu <tanjinamumu@gmail.com>
2023-01-24 23:04:33 +0600

    added

---
+++ codechef/FIT.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t,x,y,z;
+	cin>>t;
+	while(t--)
+	{
+	    cin>>x;
+	    y=x*2;
+	    z=y*5;
+	    cout<<z<<endl;
+	}
+	return 0;
+}
