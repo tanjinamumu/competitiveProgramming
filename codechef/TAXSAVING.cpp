158991b588790a21891f58868eea8ca55ef2c718
tanjinamumu <tanjinamumu@gmail.com>
2023-01-26 22:44:46 +0600

    added

---
+++ codechef/TAXSAVING.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t,x,y;
+	cin>>t;
+	while(t--)
+	{
+	    cin>>x>>y;
+	    cout<<abs(x-y)<<endl;
+	}
+	return 0;
+}
