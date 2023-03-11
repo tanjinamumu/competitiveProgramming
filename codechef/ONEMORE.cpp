7fdce0ef1b8703ad3efbfe98c5f2647dfd910e1d
tanjinamumu <tanjinamumu@gmail.com>
2023-03-11 20:40:28 +0600

    added

---
+++ codechef/ONEMORE.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t,x;
+	cin>>t;
+	while(t--)
+	{
+	    cin>>x;
+	    if(x>24)
+	    {
+	        cout<<"YES"<<endl;
+	    }
+	    else
+	    {
+	        cout<<"NO"<<endl;
+	    }
+	}
+	return 0;
+}
