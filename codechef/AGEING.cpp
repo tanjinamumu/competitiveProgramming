6263c0d3f3633c4d78fd88f556e70bdfa2a080a5
tanjinamumu <tanjinamumu@gmail.com>
2023-03-18 18:49:35 +0600

    added

---
+++ codechef/AGEING.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t,x,s;
+	cin>>t;
+	while(t--)
+	{
+	    cin>>x;
+	    s=x-10;
+	    cout<<s<<endl;
+	}
+	return 0;
+}
