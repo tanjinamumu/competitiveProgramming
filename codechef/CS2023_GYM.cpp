17654a8681b27adc864ac628e29072a9cfb26eb6
tanjinamumu <tanjinamumu@gmail.com>
2023-06-22 08:50:29 +0600

    new file added

---
+++ codechef/CS2023_GYM.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t,a,b,x,y,c,d;
+	cin>>t;
+	while(t--)
+	{
+	    cin>>x>>y;
+	    c=y/2;
+	    d=c+1;
+	    cout<<d<<endl;
+	}
+	return 0;
+}
