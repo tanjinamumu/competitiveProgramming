ecbe9e2c56f8e0a24dc097edcfdceb6b4aaae762
tanjinamumu <tanjinamumu@gmail.com>
2023-06-22 08:51:49 +0600

    added

---
+++ codechef/DONDRIVE.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t,x,y,c,d;
+	cin>>t;
+	while(t--)
+	{
+	    cin>>x>>y;
+	    c=abs(x-y);
+	    cout<<c<<endl;
+	}
+	return 0;
+}
