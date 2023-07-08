eda9215a4356693c47849e49e82acccb9705e9e8
tanjinamumu <tanjinamumu@gmail.com>
2023-07-08 11:53:37 +0600

    skip numbers added

---
+++ Simple_Codes_For_Interview/skipNumbers.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    for(int i=1;i<=10;i++)
+    {
+        if(i==3 || i==4 || i==8)
+        {
+            continue;
+        }
+        cout<<i<<" ";
+    }
+}
+
